5e9dbb3b67c15f8811e34f982a98f2842cd226e4
Manuel Troncoso <git.manuel98@gmail.com>
2020-11-10 23:20:27 +0100

    Primer bloque de videos del curso

---
+++ src/components/dish.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export class Flag extends Component
+{
+    render(){
+        return (
+            <div>
+                <h1>Bandera</h1>
+            </div>
+        );
+    }
+}
+
+export class Ingredient extends Component
+{
+    render(){
+        return (
+            <div>
+                <h1>Ingredientes</h1>
+            </div>
+        );
+    }
+}
+
+class Dish extends Component
+{
+    render(){
+        return (
+            <div>
+                <h1>Platillos</h1>
+                <Ingredient/>
+            </div>
+        );
+    }
+}
+export default Dish;
